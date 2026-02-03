bc19e943e347f2143172d1e32c22bdbe2a17c951
MEPPERDONAS <fabiansd1706@gmail.com>
2026-02-03 15:43:24 -0500

    mejoras visuales

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  // 'selector' (o 'class' en versiones antiguas) obliga a Tailwind 
-  // a buscar la clase .dark en el HTML en lugar de preguntar al navegador.
   darkMode: 'selector', 
   theme: {
     extend: {},
