d585429279c415d77b170997a585971619971ac9
MEPPERDONAS <fabiansd1706@gmail.com>
2026-02-05 12:49:14 -0500

    pages 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 export default defineConfig({
 
-  base: isGitHubPages ? '/mypersonalweb' : '/',
- 
   site: 'https://MEPPERDONAS.github.io',
   
   vite: {
