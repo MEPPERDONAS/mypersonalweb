b90ec8285ece7232a5fcb20889b9f62a04c1adc3
MEPPERDONAS <fabiansd1706@gmail.com>
2026-02-05 12:56:28 -0500

    vercel variant 3,14

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  
-  base: '/mypersonalweb',
-  site: 'https://MEPPERDONAS.github.io',
+
+
   
   vite: {
     plugins: [tailwindcss()]
