ac846320782e5255a494f6a324497105e3b2d4f8
MEPPERDONAS <fabiansd1706@gmail.com>
2026-02-05 10:42:08 -0500

    vercel variant 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
+
 
 const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 export default defineConfig({
@@ -9,7 +9,7 @@ export default defineConfig({
   base: isGitHubPages ? '/mypersonalweb' : '/',
  
   site: 'https://MEPPERDONAS.github.io',
-  adapter: vercel(),
+  
   vite: {
     plugins: [tailwindcss()]
   }
